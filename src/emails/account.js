f53713ae9164614754c55c4dfe64bf691ba4c76e
Avi Bansal <avibansal005@gmail.com>
2020-10-24 11:26:31 +0530

    Task Manager

---
+++ src/emails/account.js
@@ -0,0 +1,26 @@
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_API_Key)
+
+const sendWelcomeEmail = (email,name) => {
+    sgMail.send({
+        to: email,
+        from: 'avibansal005@gmail.com',
+        subject: 'Thanks for joining in',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+}
+
+const sendCancelationEmail = async (email,name) => {
+    await sgMail.send({
+        to: email,
+        from: 'avibansal005@gmail.com',
+        subject: 'Sorry to see you go',
+        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
